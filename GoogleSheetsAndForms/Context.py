41482bc79bcd789f4aa0bf033149ff6569c3831d
Psicognomo <37918620+Psicognomo@users.noreply.github.com>
2021-04-23 12:39:35 -0700

    Update Context.py

--- GoogleSheetsAndForms/Context.py
+++ GoogleSheetsAndForms/Context.py
@@ -15,7 +15,7 @@ class Context:
         if not isinstance(objName, str):
             raise Exception(FAIL("Cannot Retrieve Non-String-Keyed Object From Context: {}!".format(objName)))
 
-        output = self.__CTX.get(objName, None)
+        output = self.CONTEXT.get(objName, None)
         if output is None:
             raise Exception(FAIL("Context has no {0} object!".format(objName)))
         return output
@@ -25,7 +25,7 @@ class Context:
             raise Exception(FAIL("Storing key MUST be a string!"))
         if obj is None:
             raise Exception(FAIL("Stored object MUST NOT be None!"))
-        if self.__CTX.get(objName, None) is not None:
+        if self.CONTEXT.get(objName, None) is not None:
             raise Exception(FAIL("Cannot over-write object in Context!"))
 
         self.__CTX[objName] = obj
